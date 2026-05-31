a07272a53072567c3d1367310ae59c87ef99a5e2
CloneWith <clonewith@qq.com>
2026-05-31 15:34:21 +0800

    layout(navbar): add coding and CTF entries

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,4 +2,15 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
+  "/intro",
+  {
+    text: "代码",
+    icon: "code",
+    link: "/posts/code/",
+  },
+  {
+    text: "CTF",
+    icon: "bullseye",
+    link: "/posts/security/",
+  },
 ]);
